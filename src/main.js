c23be535967d09fdb01dddaa181bdb4eca67d77e
贾森 <773881048@qq.com>
2021-04-26 15:02:34 +0800

    fix: 增加注释

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store';
-import '@/utils/rem.js'
+// 移动端开发引用,用于缩放全屏
+// import '@/utils/rem.js'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'bootstrap';
