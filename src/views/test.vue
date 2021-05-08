f1fcb078b9bd9e0b94145cf71329db7b9bc02062
贾森 <773881048@qq.com>
2021-05-08 14:49:23 +0800

    feature: 增加前端校验码

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    test
+    <el-input type="text" v-model="verify"></el-input>
+    <VueImageVerify ref="verifyRef"/>
+    <el-button @click="checkVerify">校验</el-button>
   </div>
 </template>
 
 <script>
+import VueImageVerify from '../components/VueImageVerify'
+import { ref } from '@vue/runtime-core';
 export default {
-
+  components: {
+    VueImageVerify
+  },
+  setup() {
+    const verifyRef = ref(null);
+    const verify = ref('');
+    const checkVerify = () => {
+      console.log(verify.value.toLowerCase());
+      console.log(verifyRef.value.imgCode.toLowerCase());
+    }
+    return {
+      verify,
+      verifyRef,
+      checkVerify
+    }
+  }
 };
 </script>
 
